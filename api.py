85660bbdc6a2226562ca7d8daaadd0839f17f37e
Adaptz <84076017+Adaptz@users.noreply.github.com>
2023-09-10 21:19:27 -0500

    stats for mods testing

--- api.py
+++ api.py
@@ -15,6 +15,7 @@ itemsName = []
 itemsId = []
 itemsUrlName = []
 
+# Gathers id, url_name, and url_name for all items
 while(index != itemsLength):
     urlName = r.json()['payload']['items'][index]['url_name']
     id = r.json()['payload']['items'][index]['id']
@@ -31,13 +32,38 @@ error2 = 0
 error3 = 0
 error4 = 0
 error5 = 0
-print(r.status_code)
+error6 = 0
+error7 = 0
 
 listLength = len(itemsId)
 ordersAvgPlat = []
 ordersLastSold = []
+ordersItemType = []
+ordersMaxMod = []
+ordersUnrankedMod = []
 
 print('itemIdLength: ', len(itemsId), 'itemNameLength: ', len(itemsName), 'urlNameLength: ', len(itemsUrlName))
+
+
+def fetchAvgPlat(data, ordersLength, error):
+    try:    
+        avgPlat = data['payload']['statistics_closed']['90days'][ordersLength]['moving_avg']
+    except KeyError as key:
+        # Handle key error when the expected JSON structure is not found
+        avgPlat = None
+        error += 1
+        print(f"    JSON structure error: {key} (Item {index})")
+    return avgPlat, error
+
+def fetchLastSold(data, ordersLength, error):
+    try:
+        lastSold = data['payload']['statistics_closed']['90days'][ordersLength]['wa_price']
+    except KeyError as key:
+        lastSold = None
+        error += 1
+        print(f"    JSON structure error: {key} (Item {index})")
+    return lastSold, error
+    
 
 while index != listLength:
     try:
@@ -48,28 +74,41 @@ while index != listLength:
         data = r.json()
         
         if 'payload' in data and 'statistics_closed' in data['payload'] and '90days' in data['payload']['statistics_closed']:
-            ordersLength = len(r.json()['payload']['statistics_closed']['90days'])
+            ordersLength = len(r.json()['payload']['statistics_closed']['90days']) - 1
             if ordersLength > 0:
-                try:
-                    lastSold = data['payload']['statistics_closed']['90days'][ordersLength - 1]['wa_price']
-                    ordersLastSold.append(lastSold)
-                except KeyError as key:
-                    lastSold = None
-                    ordersLastSold.append(lastSold)
-                    error1 += 1
-                    print(f"    JSON structure error: {key} (Item {index})")
+                
+                avgPlat, error1 = fetchAvgPlat(data, ordersLength, error1)
+                ordersAvgPlat.append(avgPlat)
+                
+                lastSold, error2 = fetchLastSold(data, ordersLength, error2)
+                ordersLastSold.append(lastSold)
+                
+                if 'mod_rank' in (data['payload']['statistics_closed']['90days'][ordersLength] or data['payload']['statistics_closed']['90days'][int((ordersLength)/2)]):
+                    # Checks if the mod_rank object is in the indexed item data for both the unranked, [ordersLength], or "maxed", int((ordersLength)/2)],  mod
+                    ordersItemType.append('Mod')
+                    if(data['payload']['statistics_closed']['90days'][ordersLength]['mod_rank'] > 0):
+                        # Checks if the indexed mod is actually maxed
+                        avgMaxedPlat, error6 = fetchAvgPlat(data, ordersLength, error1)
+                        ordersMaxMod.append(avgPlat)
+                
+                        lastSoldMaxed, error7 = fetchLastSold(data, ordersLength, error2)
+                        ordersMaxMod.append(lastSoldMaxed)
+                    else:
+                        ordersMaxMod.append(None)
+                    if(data['payload']['statistics_closed']['90days'][ordersLength]['mod_rank'] == 0):
+                        # Checks if the indexed mod is actually unranked
+                        unrankedModPlat = data['payload']['statistics_closed']['90days'][int((ordersLength)/2)]
+                        ordersUnrankedMod.append(unrankedModPlat)
+                    else:
+                        ordersUnrankedMod.append(None)
+                else:
+                    #Handle the case when the item type isn't a mod
+                    ordersItemType.append('Item')
+                    ordersMaxMod.append('N.A.')
+                    ordersUnrankedMod.append('N.A.')
                     
-                try:    
-                    avgPlat = data['payload']['statistics_closed']['90days'][ordersLength - 1]['moving_avg']
-                    ordersAvgPlat.append(avgPlat)
-                except KeyError as key:
-                    # Handle key error when the expected JSON structure is not found
-                    avgPlat = None
-                    ordersAvgPlat.append(avgPlat)
-                    error1 += 2
-                    print(f"    JSON structure error: {key} (Item {index})")
-
-                print(f"Item {index}: {ordersUrl} - avgPlat: {avgPlat}  lastSold: {lastSold}")
+                    
+                print(f"Item {index}: {ordersUrl} - avgPlat: {avgPlat} | lastSold: {lastSold}")
             else:
                 # Handle the case when there is no data for the item
                 lastSold = None
@@ -96,7 +135,6 @@ while index != listLength:
         error5 += 1
         print(f"Item {index}: {ordersUrl} - Error fetching data: {e}")
     
-    
     index += 1
     
 print('itemIdLength: ', len(itemsId), 'itemNameLength: ', len(itemsName), 'avgPlatLength: ', len(ordersAvgPlat), 'lastSoldLength: ', len(ordersLastSold))
@@ -106,9 +144,7 @@ print(df)
 
 df.to_csv('items.csv')
 
-#avgPlatMissing = ordersAvgPlat.count(None)
 print('avgPlat fetch errors: ', error1)
-#lastSoldMissing = ordersLastSold.count(None)
 print('lastSold fetch errors: ', error2)
 print('No data errors: ', error3)
 print('Data format errors: ', error4)
@@ -116,4 +152,4 @@ print('Network errors: ', error5)
 
 end_time = time.time()
 execution_time = end_time - start_time
-print('\nProgram execution time: ', execution_time/60 ,"minutes")
+print('\nProgram execution time:', execution_time ,"seconds", execution_time/60 ,"minutes", (execution_time/60)/60 ,"hours")
